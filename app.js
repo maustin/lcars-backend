e16f0d9913fc0ee9fabdad3ae5af670cff530fa6
Mike Austin <mikeaustin@ga-sf>
2019-11-22 16:11:37 -0800

    Setup the rest of the routes

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ let app = express();
 
 let speciesRouter = require('./models/species/router');
 let ranksRouter = require('./models/ranks/router');
-//let shipsRouter = require('./models/ships/router');
-//let charactersRouter = require('./models/characters/router');
+let shipsRouter = require('./models/ships/router');
+let charactersRouter = require('./models/characters/router');
 
 app.use(express.json());
 app.use('/species', speciesRouter);
 app.use('/ranks', ranksRouter);
-//app.use('/ships', shipsRouter);
-//app.use('/characters', charactersRouter);
+app.use('/ships', shipsRouter);
+app.use('/characters', charactersRouter);
 
 app.get('/', (request, response, next) => {
 	response.send("Ohai!");
--- models/ranks/router.js
+++ models/ranks/router.js
@@ -5,22 +5,40 @@ let router = express.Router();
 
 router.get('/', (request, response, next) => {
 	model.readAll((error, data) => {
-		if (error)
-			next(error);
-		else {
-			response.json(data);
-		}
+		if (error) next(error);
+		else response.json(data);
 	});
 });
 
 router.get('/:id', (request, response, next) => {
 	model.readOne(request.params.id, (error, data) => {
-		if (error)
-			next(error);
-		else {
-			response.json(data);
-		}
-	})
-})
+		if (error) next(error);
+		else response.json(data);
+	});
+});
+
+// delete
+router.delete('/:id', (request, response, next) => {
+	model.remove(request.params.id, (error, data) => {
+		if (error) next(error);
+		else response.sendStatus(200);
+	});
+});
+
+// create
+router.post('/', (request, response, next) => {
+	model.create(request.body, (error, data) => {
+		if (error) next(error);
+		else response.sendStatus(200);
+	});
+});
+
+// update
+router.put('/', (request, response, next) => {
+	model.update(request.body, (error, data) => {
+		if (error) next(error);
+		else response.sendStatus(200);
+	});
+});
 
 module.exports = router;
